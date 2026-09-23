95a9257ee3f107fac3d6de1efa1587450473318c
Crash <1903291+hex22a@users.noreply.github.com>
2026-09-23 15:01:32 +0400

    chore: Init native SwiftUI app

---
+++ ios/saitama/saitama/saitamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  saitamaApp.swift
+//  saitama
+//
+//  Created by Albert Fazullin on 23.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct saitamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
